c9b7351ab77b6171f69ae16e579abf50fb80e833
Andrii Albu <andrew.albou@gmail.com>
2016-07-28 17:51:54 +0300

    Add imanager interface

---
+++ MediaLibrary/src/MediaLibrary.Interfaces/Managers/IManagerVersion1.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Threading.Tasks;
+
+namespace MediaLibrary.Interfaces.Managers
+{
+    interface IManagerFirstVesion
+    {
+        /// <summary>
+        /// Get resource by id
+        /// </summary>
+        /// <param name="id"></param>
+        /// <returns></returns>
+        bool GetResourse(Int32 id);
+
+        IEnumerable<BasicResourse> GetResoursesByMediaType(MediaResourseType resourceMediaType);
+        IEnumerable<BasicResourse> GetResoursesByCategory(MediaCategoryType category);
+        IEnumerable<BasicResourse> GetFavoriteResourses();
+        IEnumerable<BasicResourse> GetFavoriteResoursesByMediaType(MediaResourseType resourceMediaType);
+        IEnumerable<BasicResourse> GetFavoriteResoursesByCategory(MediaCategoryType resourceMediaType);
+
+        bool DeleteResourse(Int32 id);
+        bool DeleteResoursesByMediaType(MediaResourseType resourceMediaType);
+        bool DeleteResoursesByCategory(MediaCategoryType category);
+        bool DeleteUnFavoriteResources();
+
+        bool AddResource(BasicResourse recource);
+        IEnumerable<BasicResourse> AddResources(List<BasicResourse> recources);
+
+        bool UpdateResource(BasicResourse recource);
+        IEnumerable<BasicResourse> UpdateResources(List<BasicResourse> recources);
+
+
+        MediaCategoryType AddResourceCategory();
+    }
+}
